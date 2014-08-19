fcb244f42641e0f3d672dbeafe91b1bd62dd5e7f
forlovas <forlovas@gmail.com>
2014-08-19 13:21:54 -0300

    Moved sqlite database outside of main source tree

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -10,7 +10,8 @@ class NewVisitorTest(StaticLiveServerCase):
   def setUpClass(cls):
     for arg in sys.argv:
       if 'liveserver' in arg:
-        cls.server_url = 'http://' + arg.split('=')[1]
+        #cls.server_url = 'http://' + arg.split('=')[1]
+        cls.server_url = arg.split('=')[1]
         return
     super().setUpClass()
     cls.server_url = cls.live_server_url
